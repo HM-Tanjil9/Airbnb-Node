29dd56ef008c0bacd90f179551f6c769c14043dc
HM-Tanjil9 <rtajil0@gmail.com>
2026-04-16 08:31:49 +0600

    modified configuration

--- AuthInGo/app/application.go
+++ AuthInGo/app/application.go
@@ -6,13 +6,27 @@ import (
 	"time"
 )
 
-// Config holds the configuration for the server
+// Config holds the configuration for the application
 type Config struct {
 	Addr string
 }
 
 type Application struct {
 	Config Config
+}
+
+// construct for config
+func NewConfig(addr string) Config{
+	return Config{
+		Addr: addr,
+	}
+}
+
+// construct for application
+func NewApplication(cfg Config) *Application{
+	return &Application{
+		Config: cfg,
+	}
 }
 
 func (app *Application) Run() error {
