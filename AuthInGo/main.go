9b61283be010503580a632d7fde26f3abdc94c3a
HM-Tanjil9 <rtajil0@gmail.com>
2026-04-29 21:42:38 +0600

    Added sql database

--- AuthInGo/main.go
+++ AuthInGo/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"AuthInGo/app"
+	config "AuthInGo/config/db"
 )
 
 func main() {
 	cfg := app.NewConfig()
 	app := app.NewApplication(cfg)
+	config.SetupDB()
 	app.Run()
 }
